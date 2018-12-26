5b4942f5a639dedc17847147574d333a5abdfd2f
Ramon Blanquer <blanquer.ramon@gmail.com>
2018-12-26 17:29:14 +0000

    Stack ready for local development and docker compose.

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  // The frontend greets with the following:
+  const message = "Hello from the frontend..."
+
+  // The backend should greet us with "Hello from the backend."
+  request('http://ecsfs.backend:5000', (err, response, body) => {
+    // We send both greetings together!
+    res.send(message + " " + body);
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
